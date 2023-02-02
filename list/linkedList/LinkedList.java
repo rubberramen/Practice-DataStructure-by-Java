1f946ce3e194d093e236bc30e5f1a791b99a59a4
Chanhyun Wang <rubberramen@naver.com>
2023-02-02 09:30:47 +0900

    Linked List add() method

--- list/linkedList/LinkedList.java
+++ list/linkedList/LinkedList.java
@@ -2,7 +2,7 @@ package dataStructurePrac.list.linkedList;
 
 import dataStructurePrac.list.ListInterfaceGeneric;
 
-public class LinkedList<E> implements ListInterfaceGeneric {
+public class LinkedList<E> implements ListInterfaceGeneric<E> {
 
     private Node<E> head;
     private int numItems;
@@ -12,38 +12,44 @@ public class LinkedList<E> implements ListInterfaceGeneric {
         head = new Node<>(null, null);
     }
 
+    // 연결 리스트에 원소 x 삽입
     @Override
-    public void add(int index, Object x) {
+    public void add(int index, E item) {
+        if (index >= 0 && index <= numItems) {
+            Node<E> prevNode = getNode(index - 1);
+            Node<E> newNode = new Node<>(item, prevNode.next);
+            prevNode.next = newNode;
+            numItems++;
+        }
+    }
+
+    @Override
+    public void append(E x) {
 
     }
 
     @Override
-    public void append(Object x) {
-
-    }
-
-    @Override
-    public Object remove(int index) {
+    public E remove(int index) {
         return null;
     }
 
     @Override
-    public boolean removeItem(Object x) {
+    public boolean removeItem(E x) {
         return false;
     }
 
     @Override
-    public Object get(int index) {
+    public E get(int index) {
         return null;
     }
 
     @Override
-    public void set(int index, Object x) {
+    public void set(int index, E x) {
 
     }
 
     @Override
-    public int indexOf(Object x) {
+    public int indexOf(E x) {
         return 0;
     }
 
@@ -60,5 +66,27 @@ public class LinkedList<E> implements ListInterfaceGeneric {
     @Override
     public void clear() {
 
+    }
+
+    //////////////////////////////////////////////////////
+    public Node<E> getNode(int index) {
+        if (index >= -1 && index <= numItems - 1) {
+            Node<E> currNode = head;  	// 더미 노드
+            for (int i = 0; i <= index; i++) {
+                currNode = currNode.next;
+            }
+            return currNode;
+        } else {
+            return null;
+        }
+    }
+
+    public void printAll() {
+        Node<E> t;
+        System.out.print("Print list (#items=" + numItems + ") ");
+        for(t=head.next; t != null; t = t.next) {
+            System.out.print(t.item + " ");
+        }
+        System.out.println();
     }
 }
